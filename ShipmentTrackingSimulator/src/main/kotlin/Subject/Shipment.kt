48dcf0c691b680ce9ce2a22996ed05794cf09cfd
Melanie Pena <penamelanie3@gmail.com>
2022-05-27 15:07:51 -0600

    code review fixes: put notifyObservers method in all attribute setters

--- ShipmentTrackingSimulator/src/main/kotlin/Subject/Shipment.kt
+++ ShipmentTrackingSimulator/src/main/kotlin/Subject/Shipment.kt
@@ -2,20 +2,31 @@ package subject
 
 import observer.TrackerViewHelper
 import shippingUpdate.ShippingUpdate
-class Shipment(val id: String, var status: String): Subject {
+class Shipment(val id: String, status: String): Subject {
     val observers: MutableList<TrackerViewHelper> = mutableListOf()
+    var status: String = status
+        set(value) {
+            field = value
+            notifyObservers()
+        }
     var currentLocation: String = "unknown"
+        set(value) {
+            field = value
+            notifyObservers()
+        }
     var expectedDeliveryDate: Long = 0
         set(value) {
             if (value < 0){
                 throw Exception("Expected delivery date cannot be a negative time.")
             }
             field = value
+            notifyObservers()
         }
     val notes: MutableList<String> = mutableListOf()
     val updateHistory: MutableList<ShippingUpdate> = mutableListOf()
     fun addNote(note: String){
         notes.add(note)
+        notifyObservers()
     }
     fun addUpdate(shippingUpdate: ShippingUpdate){
         updateHistory.add(shippingUpdate)
